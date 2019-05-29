9340b8ac333db84f25fed585d9eab949be7eda1d
itspanicky <nicky.li92@gmail.com>
2019-05-29 18:36:52 -0400

    Style navbar

--- frontend/components/application.jsx
+++ frontend/components/application.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
 
 const App = () => {
     return(
         <div className="app">
             <header>
-                <h1>ummblr main page</h1>
-                <GreetingContainer />
+                {/* <GreetingContainer /> */}
             </header>
 
+            <Route exact path="/" component={GreetingContainer} />
             <AuthRoute path="/signup" component={SignupFormContainer} />
             <AuthRoute path="/login" component={LoginFormContainer} />
 
