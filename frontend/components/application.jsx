a6477197d268a119147d364747f1a77e66399f99
itspanicky <nicky.li92@gmail.com>
2019-05-31 12:30:20 -0400

    Set up modal

--- frontend/components/application.jsx
+++ frontend/components/application.jsx
@@ -5,11 +5,12 @@ import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-
+import Modal from './modal/modal'
 
 const App = () => {
     return(
         <div className="app">
+            <Modal/>
             <header>
                 {/* <GreetingContainer /> */}
             </header>
--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,6 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { closeModal } from '../../actions/modal_actions';
 
-const Modal = ({modal, closeModal}) {
+import { CreatePostContainer } from '../posts/create_post_form_container';
 
+const Modal = ({modal, closeModal}) => {
+    if (!modal) {
+        return null;
+    }
+
+    let component;
+    switch (modal) {
+        case 'Text Form':
+            component = <CreatePostContainer />;
+            break;
+        default:
+            return null;
+    }
+    return (
+        <div className="modal-background" onClick={closeModal}>
+            <div className="modal-child" onClick={e => e.stopPropagation()}>
+                { component }
+            </div>
+        </div>
+    )
 }
-export default Modal;
+
+
+
+const msp = (state) => {
+    return ({
+        modal: state.ui.modal
+    })
+}
+
+const mdp = (dispatch) => {
+    return ({
+        closeModal: () => dispatch(closeModal())
+    })
+}
+
+export default connect(msp, mdp)(Modal);
