48f621ad4cb53653ac4291d08d99062cf60a12ba
itspanicky <nicky.li92@gmail.com>
2019-06-23 23:18:25 -0400

    Fix styling of ui

--- frontend/components/navbar/create_post_modal.jsx
+++ frontend/components/navbar/create_post_modal.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { closeModal } from '../../actions/modal_actions';
 
 const CreatePostModal = ({openModal, closeModal}) => {
-
     return (
         <>
             <ul className="alt-post-buttons">
                 <li>
-                    <div onClick={() => openModal('Create Text Form')}><p className="post_button text">Aa</p><span>Text</span></div>
+                    <div onClick={() => openModal('Create Text Form')}><p className="alt post_button text">Aa</p><span>Text</span></div>
                 </li>
                 <li>
-                    <div onClick={() => openModal('Create Photo Form')}><i className="post_button fas fa-camera"></i><span>Photo</span></div>
+                    <div onClick={() => openModal('Create Photo Form')}><i className="alt post_button fas fa-camera"></i><span>Photo</span></div>
                 </li>
                 <li>
-                    <div onClick={() => openModal('Create Quote Form')}><i className="post_button fas fa-quote-right"></i><span>Quote</span></div>
+                    <div onClick={() => openModal('Create Quote Form')}><i className="alt post_button fas fa-quote-right"></i><span>Quote</span></div>
                 </li>
                 <li>
-                    <div onClick={() => openModal('Create Link Form')}><i className="post_button fas fa-link"></i><span>Link</span></div>
+                    <div onClick={() => openModal('Create Link Form')}><i className="alt post_button fas fa-link"></i><span>Link</span></div>
                 </li>
             </ul>
         </>
